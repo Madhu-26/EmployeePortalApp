f924d83e5dc4fceae74cdcdc277878dd2687b98b
Madhusha <madhushadavidmathivannan@gmail.com>
2021-12-01 16:36:44 +0530

    Sort & Filter Employee

--- angularfrontend/src/app/employee/show-emp/show-emp.component.ts
+++ angularfrontend/src/app/employee/show-emp/show-emp.component.ts
@@ -16,6 +16,10 @@ export class ShowEmpComponent implements OnInit {
   ActivateAddEditEmpComp:boolean=false; 
   emp:any;
 
+  EmployeeIDFilter: string = " ";
+  EmployeeNameFilter: string = " ";
+  EmployeeListWithoutFilter: any=[];
+
   ngOnInit(): void {
     this.refreshEmpList();
   }
@@ -45,7 +49,6 @@ export class ShowEmpComponent implements OnInit {
       this.service.deleteEmployee(item.EmployeeID).subscribe(data=>{
         alert(data.toString());
         this.refreshEmpList();
-        console.log("helloooooooo")
       })
     }
   }
@@ -58,6 +61,32 @@ export class ShowEmpComponent implements OnInit {
   refreshEmpList(){
     this.service.getEmpList().subscribe(data=>{
         this.EmployeeList=data;
+        this.EmployeeListWithoutFilter=data;
     })
     }
+  
+  FilterFn(){
+    var EmployeeIDFilter = this.EmployeeIDFilter;
+    var EmployeeNameFilter = this.EmployeeNameFilter;
+
+    this.EmployeeList = this.EmployeeListWithoutFilter.filter(function (el: any){
+      return el.EmployeeID.toString().toLowerCase().includes(
+        EmployeeIDFilter.toString().trim().toLowerCase()) && 
+        el.EmployeeName.toString().toLowerCase().includes(
+        EmployeeNameFilter.toString().trim().toLowerCase())
+    });
+  }
+
+  sortResult(prop: any,asc: any){
+    this.EmployeeList = this.EmployeeListWithoutFilter.sort(
+      function(a: any,b : any){
+        if (asc) {
+          return (a[prop]>b[prop])?1:(a[prop]<b[prop]?-1:0);
+        }else{
+          return (b[prop]>a[prop])?1:(b[prop]<a[prop]?-1:0);
+        }
+      }
+    )
+  }
+
 }
